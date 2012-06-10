8be594c0c36c075fac07de94d008dc2d3cb1cf53
Bob Warwick <warwick@Bobs-MacBook-Air.local>
2012-06-09 17:50:26 -0700

    Initial Commit

---
+++ rtfpaste/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  rtfpaste
+//
+//  Created by Bob Warwick on 12-06-09.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
