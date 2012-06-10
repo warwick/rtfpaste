e20e3eaa3ab47f2bf7688f297a61b56eb9ce6204
Bob Warwick <warwick@Bobs-MacBook-Air.local>
2012-06-09 18:12:36 -0700

    Utility to paste out RTF version of what's in the clipboard

--- rtfpaste/main.m
+++ rtfpaste/main.m
@@ -7,14 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+                
+        NSArray *attributedStrings = [[NSPasteboard generalPasteboard] readObjectsForClasses:[NSArray arrayWithObject:[NSAttributedString class]] options:nil];
+        if ([attributedStrings count] == 0) {
+            NSLog(@"Error reading attributed string");
+            return 1;
+        }
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSAttributedString *attributed = [attributedStrings objectAtIndex:0];
+        NSData *rtfData = [attributed RTFFromRange:NSMakeRange(0, [attributed length]) documentAttributes:nil];
+        printf("%s", [[[NSString alloc] initWithData:rtfData encoding:NSASCIIStringEncoding] UTF8String]);
         
     }
     return 0;
